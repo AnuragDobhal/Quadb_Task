b771509e8aeaaa3fc4afe6dbea3f167a18e7da94
AnuragDobhal <dobhalannu246@gmail.com>
2023-06-05 22:23:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link, useParams } from 'react-router-dom';
+import axios from 'axios';
+import './Style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="container">
+        <h1>TV Shows</h1>
+        <Routes>
+          <Route path="/" element={<ShowList />} />
+          <Route path="/shows/:id" element={<ShowDetails />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function ShowList() {
+  const [shows, setShows] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://api.tvmaze.com/search/shows?q=all')
+      .then((response) => {
+       setShows(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <ul className="show-list">
+      {shows.map((show) => (
+        <li key={show.show.id}>
+          <Link to={`/shows/${show.show.id}`} className="show-name">
+            {show.show.name}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function ShowDetails() {
+  const { id } = useParams();
+  const [show, setShow] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`https://api.tvmaze.com/shows/${id}`)
+      .then((response) => {
+        setShow(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [id]);
+
+  return (
+    <div>
+      {show && (
+        <div className="show-details">
+          <h2>{show.name}</h2>
+          <div className="show-summary" dangerouslySetInnerHTML={{ __html: show.summary }} />
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/ShowData.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ShowingData = ({ summary }) => {
+  return (
+    <div>
+      <h2>Show Details</h2>
+      <p>{summary}</p>
+    </div>
+  );
+}
+
+export default ShowingData;
